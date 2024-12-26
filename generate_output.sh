f06f9cb27e50473ccc43c9d058b2d5f3b6952ca8
daniel <daniel@fearnley.net>
2024-12-26 16:06:16 +0000

    Update output script with line count and add missing pleco flag

--- generate_output.sh
+++ generate_output.sh
@@ -1,2 +1,7 @@
 java -cp out/production/CC-CEDICT-additions src.main.java.Main > output/output_cedict_format.txt
-java -cp out/production/CC-CEDICT-additions src.main.java.Main > output/output_pleco_format.txt
+java -cp out/production/CC-CEDICT-additions src.main.java.Main -f pleco > output/output_pleco_format.txt
+cedict_line_count=$(wc -l output/output_cedict_format.txt)
+pleco_line_count=$(wc -l output/output_pleco_format.txt)
+echo "Line counts:"
+echo "$cedict_line_count"
+echo "$pleco_line_count"
