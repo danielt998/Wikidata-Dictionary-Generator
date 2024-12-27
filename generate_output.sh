fe7df84915c58bb03e5ae85ccaca25284e983259
daniel <daniel@fearnley.net>
2024-12-27 15:24:31 +0000

    Provide option to output data in original tsv format (but after filtering)

--- generate_output.sh
+++ generate_output.sh
@@ -1,7 +1,11 @@
 java -cp out/production/CC-CEDICT-additions src.main.java.Main > output/output_cedict_format.txt
 java -cp out/production/CC-CEDICT-additions src.main.java.Main -f pleco > output/output_pleco_format.txt
+java -cp out/production/CC-CEDICT-additions src.main.java.Main -f all_data > output/all_data.tsv
+
 cedict_line_count=$(wc -l output/output_cedict_format.txt)
 pleco_line_count=$(wc -l output/output_pleco_format.txt)
+all_data_line_count=$(wc -l output/all_data.tsv)
 echo "Line counts:"
 echo "$cedict_line_count"
 echo "$pleco_line_count"
+echo "$all_data_line_count"
