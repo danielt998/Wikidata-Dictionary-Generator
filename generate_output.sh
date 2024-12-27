246ece67c785c231fd102f10dcb4fecc8177792b
daniel <daniel@fearnley.net>
2024-12-27 22:35:53 +0000

    start work on migrating some code to use ICU

--- generate_output.sh
+++ generate_output.sh
@@ -1,6 +1,6 @@
-java -cp out/production/CC-CEDICT-additions src.main.java.Main > output/output_cedict_format.txt
-java -cp out/production/CC-CEDICT-additions src.main.java.Main -f pleco > output/output_pleco_format.txt
-java -cp out/production/CC-CEDICT-additions src.main.java.Main -f all_data > output/all_data.tsv
+gradle run --console=plain --quiet > output/output_cedict_format.txt
+gradle run --console=plain --quiet --args="-f pleco" > output/output_pleco_format.txt
+gradle run --console=plain --quiet --args="-f all_data" > output/all_data.tsv
 
 cedict_line_count=$(wc -l output/output_cedict_format.txt)
 pleco_line_count=$(wc -l output/output_pleco_format.txt)
--- src/main/java/wikidatadictionarygenerator/Extract.java
+++ src/main/java/wikidatadictionarygenerator/Extract.java
@@ -1,4 +1,5 @@
-package src.main.java;
+package wikidatadictionarygenerator;
+
 
 import java.io.BufferedReader;
 import java.io.File;
