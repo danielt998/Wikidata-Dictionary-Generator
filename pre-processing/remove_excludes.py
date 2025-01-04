bf7413f5d66960d61a41c526ba1e23f787e28215
daniel <daniel@fearnley.net>
2025-01-04 17:58:47 +0000

    Update exclude script to ignore anything without han
    Former-commit-id: c79d3f4fd0388e5a517fcedab50be5cf84eb9e37
    Former-commit-id: c4e19c63aea672535dadadb08ece63f07cb67769

--- pre-processing/remove_excludes.py
+++ pre-processing/remove_excludes.py
@@ -1,6 +1,9 @@
 import sys
+import re
 
 orig_directory = sys.argv[1]
+
+ZH_REGEX = re.compile(r'[\u4e00-\u9fff]+')
 
 excludeList = None
 with open('resources/excludeList') as f:
@@ -16,6 +19,16 @@ for line in the_list:
         for exclude_item in excludeList:
             if a_type.strip() == exclude_item.strip():
                 exclude = True
+
+    # ignore anything with no Han text
+    # I think this may miss stuff but only obscure characters?
+    contains_zh = False
+    for field in types:
+        if re.findall(ZH_REGEX, field):
+            contains_zh = True
+    if not contains_zh:
+        exclude = True
+
     if not exclude:
         sys.stdout.write(line)
         sys.stdout.flush()
