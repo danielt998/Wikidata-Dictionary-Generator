48f86ceb9b9b11f2c42890781a7bfc6b5f2690f6
daniel <daniel@fearnley.net>
2025-02-01 12:45:07 +0000

    fix typo in generate_output.sh

--- generate_output.sh
+++ generate_output.sh
@@ -17,7 +17,7 @@ then
   gsplit -C 99m --numeric-suffixes output/output_pleco_format.txt output/output_pleco_format_split_
 elif [[ "$OSTYPE" == "darwin" ]]; then
     #This won't work if the total size becomes >200MB but Mac OS doesn't support the -C flag by default
-    split n 2 --numeric-suffixes output/output_cedict_format.txt output/output_cedict_format_split_
+    split -n 2 --numeric-suffixes output/output_cedict_format.txt output/output_cedict_format_split_
     split -n 2 --numeric-suffixes output/output_pleco_format.txt output/output_pleco_format_split_
 else
   split -C 99m --numeric-suffixes output/output_cedict_format.txt output/output_cedict_format_split_
